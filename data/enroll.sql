d5e228e60c7b114091fa9bd6ab66697dd7a89459
Allendale Nato <natoallendale@gmail.com>
2017-07-21 22:05:48 +0800

    Add data 9 out of 12

---
+++ data/enroll.sql
@@ -0,0 +1,5 @@
+INSERT INTO Enroll (student_id,class_id) VALUES (1,4),(2,7),(3,6),(4,17),(5,11),(6,3),(7,10),(8,17),(9,13),(10,5);
+INSERT INTO Enroll (student_id,class_id) VALUES (11,10),(12,5),(13,5),(14,16),(15,17),(16,8),(17,7),(18,12),(19,9),(20,16);
+INSERT INTO Enroll (student_id,class_id) VALUES (21,19),(22,14),(23,17),(24,20),(25,16),(26,11),(27,11),(28,9),(29,17),(30,5);
+INSERT INTO Enroll (student_id,class_id) VALUES (31,14),(32,19),(33,14),(34,13),(35,7),(36,14),(37,14),(38,12),(39,20),(40,6);
+INSERT INTO Enroll (student_id,class_id) VALUES (41,16),(42,5),(43,3),(44,7),(45,16),(46,8),(47,11),(48,19),(49,13),(50,11);
---
+++ data/expert.sql
@@ -0,0 +1,14 @@
+INSERT INTO Expertise (name)
+    VALUES 
+        ("Networking"),
+        ("IoT"),
+        ("Security"),
+        ("Machine Learning"),
+        ("Bioinformatics"),
+        ("Assembly"),
+        ("Web Development"),
+        ("Software Engineering"),
+        ("OOP"),
+        ("Project Management"),
+        ("Database"),
+        ("Automation")
