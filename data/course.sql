8ec99bbb0aa8af29c2e01421e4ccb317561b0ee7
Allendale Nato <natoallendale@gmail.com>
2017-07-21 22:45:48 +0800

    Change double quotes to single

--- data/course.sql
+++ data/course.sql
@@ -1,7 +1,7 @@
 INSERT INTO Course (name) 
 VALUES
-    ("THES0"),
-    ("THES1"),
-    ("THES2"),
-    ("THES3"),
-    ("THES4")
+    ('THES0'),
+    ('THES1'),
+    ('THES2'),
+    ('THES3'),
+    ('THES4')
